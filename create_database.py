40914f008b21c4605faa72caf6bf2f1964ed7ca5
JakubWilczek98 <jakubwilczek98@gmail.com>
2022-05-13 10:02:59 +0200

    Created ingrediences and recipes databases

--- create_database.py
+++ create_database.py
@@ -1,19 +1,56 @@
 import sqlite3
+import json
 
-def database_operations():
+def create_database():
     db = sqlite3.connect("food_search.db")
     cursor = db.cursor()
 
     cursor.execute('''
         CREATE TABLE ingredients(
         id integer,
-        ingr string
+        ingr string,
+        recipe integer
         )
     ''')
+
+    cursor.execute('''
+            CREATE TABLE recipes(
+            id integer,
+            recipe json
+            )
+        ''')
 
     db.commit()
     db.close()
 
+def add_to_database():
+    db = sqlite3.connect("food_search.db")
+    cursor = db.cursor()
+
+    cursor.execute(
+        "insert into ingredients values (?,?,?)",
+        [1, "['apple','pear']", 1]
+    )
+
+    db.commit()
+    db.close()
+
+def read_from_database():
+    db = sqlite3.connect("food_search.db")
+    cursor = db.cursor()
+
+    cursor.execute('''
+                SELECT * from ingredients
+            ''')
+
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+
+    db.commit()
+    db.close()
 
 if __name__ == '__main__':
-    database_operations()
+    create_database()
+    #add_to_database()
+    #read_from_database()
